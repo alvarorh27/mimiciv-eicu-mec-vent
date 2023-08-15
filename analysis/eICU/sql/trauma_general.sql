6c46fc480a92d91965f3dcfb76e78fff8f303895
Álvaro Ritoré Hidalgo <122281206+alvarorh27@users.noreply.github.com>
2023-08-15 11:54:02 +0200

    Create trauma_general.sql

---
+++ analysis/eICU/sql/trauma_general.sql
@@ -0,0 +1,38 @@
+WITH sq1 AS (
+  SELECT
+    patientunitstayid,
+    diagnosisoffset,
+    icd9code,
+    MIN(diagnosisoffset) OVER (PARTITION BY patientunitstayid) AS min_diagnosisoffset,
+    CASE
+      WHEN (icd9code LIKE 'S46____' OR icd9code LIKE 'S56____' OR icd9code LIKE 'S66____' OR icd9code LIKE 'S76____' OR icd9code LIKE 'S86____' OR icd9code LIKE 'S96____' OR icd9code LIKE 'S29____' OR icd9code LIKE 'S39____' OR icd9code LIKE 'S36____' OR icd9code LIKE 'S37____' OR icd9code LIKE 'S27____' OR icd9code LIKE 'S26____' OR icd9code LIKE 'S07____' OR icd9code LIKE 'S17____' OR icd9code LIKE 'S38____' OR icd9code LIKE 'S28____' OR icd9code LIKE 'S47____' OR icd9code LIKE 'S57____' OR icd9code LIKE 'S67____' OR icd9code LIKE 'S77____' OR icd9code LIKE 'S87____' OR icd9code LIKE 'S97____' OR icd9code LIKE 'S04____' OR icd9code LIKE 'S14____' OR icd9code LIKE 'S24____' OR icd9code LIKE 'S34____' OR icd9code LIKE 'S54____' OR icd9code LIKE 'S64____' OR icd9code LIKE 'S44____' OR icd9code LIKE 'S74____' OR icd9code LIKE 'S84____' OR icd9code LIKE 'S94____' OR icd9code LIKE 'S09____' OR icd9code LIKE 'S19____' OR icd9code LIKE 'S49____' OR icd9code LIKE 'S59____' OR icd9code LIKE 'S69____' OR icd9code LIKE 'S79____' OR icd9code LIKE 'S89____')
+           AND (icd9code LIKE '______A' OR icd9code LIKE '______B' OR icd9code LIKE '______C')
+           OR (icd9code BETWEEN '860' AND '86999' OR icd9code BETWEEN '925' AND '92999' OR icd9code BETWEEN '950' AND '95799' OR icd9code BETWEEN '9590' AND '9599')
+      THEN 'YES'
+      ELSE 'NO'
+    END AS traumatic_general_min_diagnosisoffset,
+    CASE
+      WHEN (icd9code LIKE 'S46____' OR icd9code LIKE 'S56____' OR icd9code LIKE 'S66____' OR icd9code LIKE 'S76____' OR icd9code LIKE 'S86____' OR icd9code LIKE 'S96____' OR icd9code LIKE 'S29____' OR icd9code LIKE 'S39____' OR icd9code LIKE 'S36____' OR icd9code LIKE 'S37____' OR icd9code LIKE 'S27____' OR icd9code LIKE 'S26____' OR icd9code LIKE 'S07____' OR icd9code LIKE 'S17____' OR icd9code LIKE 'S38____' OR icd9code LIKE 'S28____' OR icd9code LIKE 'S47____' OR icd9code LIKE 'S57____' OR icd9code LIKE 'S67____' OR icd9code LIKE 'S77____' OR icd9code LIKE 'S87____' OR icd9code LIKE 'S97____' OR icd9code LIKE 'S04____' OR icd9code LIKE 'S14____' OR icd9code LIKE 'S24____' OR icd9code LIKE 'S34____' OR icd9code LIKE 'S54____' OR icd9code LIKE 'S64____' OR icd9code LIKE 'S44____' OR icd9code LIKE 'S74____' OR icd9code LIKE 'S84____' OR icd9code LIKE 'S94____' OR icd9code LIKE 'S09____' OR icd9code LIKE 'S19____' OR icd9code LIKE 'S49____' OR icd9code LIKE 'S59____' OR icd9code LIKE 'S69____' OR icd9code LIKE 'S79____' OR icd9code LIKE 'S89____')
+           AND (icd9code LIKE '______A' OR icd9code LIKE '______B' OR icd9code LIKE '______C')
+           OR (icd9code BETWEEN '860' AND '86999' OR icd9code BETWEEN '925' AND '92999' OR icd9code BETWEEN '950' AND '95799' OR icd9code BETWEEN '9590' AND '9599')
+           AND patientunitstayid IN (
+                SELECT patientunitstayid
+                FROM `physionet-data.eicu_crd.diagnosis`
+                GROUP BY patientunitstayid
+                HAVING COUNT(DISTINCT diagnosisoffset) > 1
+           )
+      THEN 'YES'
+      ELSE 'NO'
+    END AS traumatic_general_any_diagnosisoffset
+  FROM
+    `physionet-data.eicu_crd.diagnosis`
+) --end of sq1
+SELECT
+    patientunitstayid,
+    diagnosisoffset,
+    icd9code,
+    traumatic_general_min_diagnosisoffset,
+    traumatic_general_any_diagnosisoffset  
+FROM sq1
+WHERE diagnosisoffset = min_diagnosisoffset AND (traumatic_general_min_diagnosisoffset = 'YES'  OR traumatic_general_any_diagnosisoffset = 'YES') --group when there is Traumatic general injury
+ORDER BY patientunitstayid ASC;

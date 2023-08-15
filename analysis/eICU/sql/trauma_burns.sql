1edaed411f3aa829b5f62a3f5e71015bee155739
Álvaro Ritoré Hidalgo <122281206+alvarorh27@users.noreply.github.com>
2023-08-15 11:52:15 +0200

    Create trauma_burns.sql

---
+++ analysis/eICU/sql/trauma_burns.sql
@@ -0,0 +1,34 @@
+WITH sq1 AS (
+  SELECT
+    patientunitstayid,
+    diagnosisoffset,
+    icd9code,
+    MIN(diagnosisoffset) OVER (PARTITION BY patientunitstayid) AS min_diagnosisoffset,
+    CASE
+      WHEN (((icd9code BETWEEN 'T20' AND 'T299') OR (icd9code BETWEEN 'T30' AND 'T329') OR (icd9code BETWEEN '94%' AND '94999')) AND icd9code NOT LIKE '9S' AND icd9code NOT LIKE '9D')
+      THEN 'YES'
+      ELSE 'NO'
+    END AS burns_min_diagnosisoffset,
+    CASE
+      WHEN (((icd9code BETWEEN 'T20' AND 'T299') OR (icd9code BETWEEN 'T30' AND 'T329') OR (icd9code BETWEEN '94%' AND '94999')) AND icd9code NOT LIKE '9S' AND icd9code NOT LIKE '9D')
+           AND patientunitstayid IN (
+                SELECT patientunitstayid
+                FROM `physionet-data.eicu_crd.diagnosis`
+                GROUP BY patientunitstayid
+                HAVING COUNT(DISTINCT diagnosisoffset) > 1
+           )
+      THEN 'YES'
+      ELSE 'NO'
+    END AS burns_any_diagnosisoffset
+  FROM
+    `physionet-data.eicu_crd.diagnosis`
+)
+SELECT
+    patientunitstayid,
+    diagnosisoffset,
+    icd9code,
+    burns_min_diagnosisoffset,
+    burns_any_diagnosisoffset  
+FROM sq1
+WHERE diagnosisoffset = min_diagnosisoffset AND (burns_min_diagnosisoffset = 'YES'  OR burns_any_diagnosisoffset = 'YES')
+ORDER BY patientunitstayid ASC;

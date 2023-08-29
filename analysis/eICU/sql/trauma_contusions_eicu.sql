c0e0d43a0cd907ef53e2551b6183a3d7a0cf82b0
claudia1501 <132444794+claudia1501@users.noreply.github.com>
2023-08-29 12:36:21 +0200

    Update trauma_contusions_eicu.sql

--- analysis/eICU/sql/trauma_contusions_eicu.sql
+++ analysis/eICU/sql/trauma_contusions_eicu.sql
@@ -3,32 +3,25 @@ WITH sq1 AS (
     patientunitstayid,
     diagnosisoffset,
     icd9code,
-    MIN(diagnosisoffset) OVER (PARTITION BY patientunitstayid) AS min_diagnosisoffset,
     CASE
-      WHEN (icd9code LIKE 'S10____' OR icd9code LIKE 'S00____' OR icd9code LIKE 'S05____' OR icd9code LIKE 'S20____' OR icd9code LIKE 'S40____' OR icd9code LIKE 'S50____' OR icd9code LIKE 'S60____' OR icd9code LIKE 'S70____' OR icd9code LIKE 'S80____' OR icd9code LIKE 'S90____' OR icd9code BETWEEN '92%' AND '92499') AND (icd9code NOT LIKE '______D'AND icd9code NOT LIKE '______S')
+      WHEN (icd9code LIKE 'S10____' OR icd9code LIKE 'S00____' OR icd9code LIKE 'S05____' OR icd9code LIKE 'S20____' OR icd9code LIKE 'S40____' OR icd9code LIKE 'S50____' OR icd9code LIKE 'S60____' OR icd9code LIKE 'S70____' OR icd9code LIKE 'S80____' OR icd9code LIKE 'S90____' OR icd9code BETWEEN '92%' AND '92499') AND (icd9code NOT LIKE '______D'AND icd9code NOT LIKE '______S') AND diagnosisoffset = min_diagnosisoffset
       THEN 1
       ELSE 0
     END AS contusion_min_diagnosisoffset,
     CASE
-      WHEN (icd9code LIKE 'S10____' OR icd9code LIKE 'S00____' OR icd9code LIKE 'S05____' OR icd9code LIKE 'S20____' OR icd9code LIKE 'S40____' OR icd9code LIKE 'S50____' OR icd9code LIKE 'S60____' OR icd9code LIKE 'S70____' OR icd9code LIKE 'S80____' OR icd9code LIKE 'S90____' OR icd9code BETWEEN '92%' AND '92499') AND (icd9code NOT LIKE '______D'AND icd9code NOT LIKE '______S')
-           AND patientunitstayid IN (
-                SELECT patientunitstayid
-                FROM `physionet-data.eicu_crd.diagnosis`
-                GROUP BY patientunitstayid
-                HAVING COUNT(DISTINCT diagnosisoffset) > 1
-           )
+      WHEN (icd9code LIKE 'S10____' OR icd9code LIKE 'S00____' OR icd9code LIKE 'S05____' OR icd9code LIKE 'S20____' OR icd9code LIKE 'S40____' OR icd9code LIKE 'S50____' OR icd9code LIKE 'S60____' OR icd9code LIKE 'S70____' OR icd9code LIKE 'S80____' OR icd9code LIKE 'S90____' OR icd9code BETWEEN '92%' AND '92499') AND (icd9code NOT LIKE '______D'AND icd9code NOT LIKE '______S') 
       THEN 1
       ELSE 0
     END AS contusion_any_diagnosisoffset
-  FROM
-    `physionet-data.eicu_crd.diagnosis`
+  FROM (
+    SELECT * ,MIN(diagnosisoffset) OVER (PARTITION BY patientunitstayid) AS min_diagnosisoffset
+    FROM 
+    `physionet-data.eicu_crd.diagnosis`) 
 )
 SELECT
     patientunitstayid AS stay_id,
-    diagnosisoffset,
-    icd9code,
     contusion_min_diagnosisoffset,
-    contusion_any_diagnosisoffset  
+    contusion_any_diagnosisoffset 
 FROM sq1
-WHERE diagnosisoffset = min_diagnosisoffset AND (contusion_min_diagnosisoffset = 1  OR contusion_any_diagnosisoffset = 1)
+WHERE contusion_min_diagnosisoffset = 1 OR contusion_any_diagnosisoffset = 1
 ORDER BY patientunitstayid ASC;

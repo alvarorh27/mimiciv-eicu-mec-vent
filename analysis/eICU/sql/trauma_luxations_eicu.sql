6ff9b88c58cb9ea6b2335dad0232b9c41b2b2304
Álvaro Ritoré Hidalgo <122281206+alvarorh27@users.noreply.github.com>
2023-08-30 12:06:48 +0200

    Update trauma_luxations_eicu.sql

--- analysis/eICU/sql/trauma_luxations_eicu.sql
+++ analysis/eICU/sql/trauma_luxations_eicu.sql
@@ -1,27 +1,35 @@
-WITH sq1 AS (
+WITH
+  sq1 AS (
   SELECT
     patientunitstayid,
     diagnosisoffset,
     icd9code,
     CASE
-      WHEN (icd9code LIKE 'S431___' OR icd9code LIKE 'S132___' OR icd9code LIKE 'S131___' OR icd9code LIKE 'S530___' OR icd9code LIKE 'S730___' OR icd9code LIKE 'S630___' OR icd9code LIKE 'S232___' OR icd9code LIKE 'S031___' OR icd9code LIKE 'S332___' OR icd9code LIKE 'S531___' OR icd9code LIKE 'S058___' OR icd9code LIKE 'S632___' OR icd9code LIKE 'S931___' OR icd9code LIKE 'S032___' OR icd9code LIKE 'S432___' OR icd9code LIKE 'S430___' OR icd9code LIKE 'S433___' OR icd9code LIKE 'S333___' OR icd9code LIKE 'S631___' OR icd9code LIKE 'S030___' OR icd9code LIKE 'S933___' OR icd9code LIKE 'S831___' OR icd9code LIKE 'S830___' OR icd9code LIKE 'S334___' OR icd9code LIKE 'S930___' OR icd9code LIKE 'S331___' OR icd9code LIKE 'S231___' OR icd9code LIKE 'S339XA' OR icd9code LIKE 'S832___' OR icd9code LIKE 'S833___') AND (icd9code NOT LIKE '______D'AND icd9code NOT LIKE '______S') OR (icd9code BETWEEN '83%' AND '83999') AND diagnosisoffset = min_diagnosisoffset
-      THEN 1
-      ELSE 0
-    END AS luxation_min_diagnosisoffset,
+      WHEN (icd9code LIKE 'S431___' OR icd9code LIKE 'S132___' OR icd9code LIKE 'S131___' OR icd9code LIKE 'S530___' OR icd9code LIKE 'S730___' OR icd9code LIKE 'S630___' OR icd9code LIKE 'S232___' OR icd9code LIKE 'S031___' OR icd9code LIKE 'S332___' OR icd9code LIKE 'S531___' OR icd9code LIKE 'S058___' OR icd9code LIKE 'S632___' OR icd9code LIKE 'S931___' OR icd9code LIKE 'S032___' OR icd9code LIKE 'S432___' OR icd9code LIKE 'S430___' OR icd9code LIKE 'S433___' OR icd9code LIKE 'S333___' OR icd9code LIKE 'S631___' OR icd9code LIKE 'S030___' OR icd9code LIKE 'S933___' OR icd9code LIKE 'S831___' OR icd9code LIKE 'S830___' OR icd9code LIKE 'S334___' OR icd9code LIKE 'S930___' OR icd9code LIKE 'S331___' OR icd9code LIKE 'S231___' OR icd9code LIKE 'S339XA' OR icd9code LIKE 'S832___' OR icd9code LIKE 'S833___') AND (icd9code NOT LIKE '______D'AND icd9code NOT LIKE '______S') OR (icd9code BETWEEN '83%' AND '83999') AND diagnosisoffset = min_diagnosisoffset THEN 1
+    ELSE
+    0
+  END
+    AS luxation_min_diagnosisoffset,
     CASE
-      WHEN (icd9code LIKE 'S431___' OR icd9code LIKE 'S132___' OR icd9code LIKE 'S131___' OR icd9code LIKE 'S530___' OR icd9code LIKE 'S730___' OR icd9code LIKE 'S630___' OR icd9code LIKE 'S232___' OR icd9code LIKE 'S031___' OR icd9code LIKE 'S332___' OR icd9code LIKE 'S531___' OR icd9code LIKE 'S058___' OR icd9code LIKE 'S632___' OR icd9code LIKE 'S931___' OR icd9code LIKE 'S032___' OR icd9code LIKE 'S432___' OR icd9code LIKE 'S430___' OR icd9code LIKE 'S433___' OR icd9code LIKE 'S333___' OR icd9code LIKE 'S631___' OR icd9code LIKE 'S030___' OR icd9code LIKE 'S933___' OR icd9code LIKE 'S831___' OR icd9code LIKE 'S830___' OR icd9code LIKE 'S334___' OR icd9code LIKE 'S930___' OR icd9code LIKE 'S331___' OR icd9code LIKE 'S231___' OR icd9code LIKE 'S339XA' OR icd9code LIKE 'S832___' OR icd9code LIKE 'S833___') AND (icd9code NOT LIKE '______D'AND icd9code NOT LIKE '______S') OR (icd9code BETWEEN '83%' AND '83999') 
-      THEN 1
-      ELSE 0
-    END AS luxation_any_diagnosisoffset
+      WHEN (icd9code LIKE 'S431___' OR icd9code LIKE 'S132___' OR icd9code LIKE 'S131___' OR icd9code LIKE 'S530___' OR icd9code LIKE 'S730___' OR icd9code LIKE 'S630___' OR icd9code LIKE 'S232___' OR icd9code LIKE 'S031___' OR icd9code LIKE 'S332___' OR icd9code LIKE 'S531___' OR icd9code LIKE 'S058___' OR icd9code LIKE 'S632___' OR icd9code LIKE 'S931___' OR icd9code LIKE 'S032___' OR icd9code LIKE 'S432___' OR icd9code LIKE 'S430___' OR icd9code LIKE 'S433___' OR icd9code LIKE 'S333___' OR icd9code LIKE 'S631___' OR icd9code LIKE 'S030___' OR icd9code LIKE 'S933___' OR icd9code LIKE 'S831___' OR icd9code LIKE 'S830___' OR icd9code LIKE 'S334___' OR icd9code LIKE 'S930___' OR icd9code LIKE 'S331___' OR icd9code LIKE 'S231___' OR icd9code LIKE 'S339XA' OR icd9code LIKE 'S832___' OR icd9code LIKE 'S833___') AND (icd9code NOT LIKE '______D'AND icd9code NOT LIKE '______S') OR (icd9code BETWEEN '83%' AND '83999') THEN 1
+    ELSE
+    0
+  END
+    AS luxation_any_diagnosisoffset
   FROM (
-    SELECT * ,MIN(diagnosisoffset) OVER (PARTITION BY patientunitstayid) AS min_diagnosisoffset
-    FROM 
-    `physionet-data.eicu_crd.diagnosis`) 
-)
+    SELECT
+      *,
+      MIN(diagnosisoffset) OVER (PARTITION BY patientunitstayid) AS min_diagnosisoffset
+    FROM
+      `physionet-data.eicu_crd.diagnosis`) )
 SELECT
-    patientunitstayid AS stay_id,
-    luxation_min_diagnosisoffset,
-    luxation_any_diagnosisoffset 
-FROM sq1
-WHERE luxation_min_diagnosisoffset = 1 OR luxation_any_diagnosisoffset = 1
-ORDER BY patientunitstayid ASC;
+  patientunitstayid AS stay_id,
+  luxation_min_diagnosisoffset,
+  luxation_any_diagnosisoffset
+FROM
+  sq1
+WHERE
+  luxation_min_diagnosisoffset = 1
+  OR luxation_any_diagnosisoffset = 1
+ORDER BY
+  stay_id
